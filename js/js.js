d86fb9a070ee90ae150399eceacb9e8166385225
Sam <skeefhaver@gmail.com>
2022-04-12 18:18:21 -0500

    Added playRound function
    this function allows one round of rock paper scissors to be played

--- js/js.js
+++ js/js.js
@@ -1,8 +1,36 @@
 const array = ['rock', 'paper', 'scissors'];
+let computerSelection = computerPlay();
+let promptSelection = prompt('Enter your choice for rock paper scissors');
+let playerSelection = promptSelection.toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
     result = array[Math.floor(Math.random()*array.length)];
     return result;
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        console.log('tie');
+    } else {
+        switch (playerSelection + computerSelection) {
+            case 'rockscissors':
+            case 'paperrock':
+            case 'scissorspaper':
+                console.log('Player Wins Round');
+                return playerScore++;
+            case 'rockpaper':
+            case 'paperscissors':
+            case 'scissorsrock':
+                console.log('Computer Wins Round');
+                return computerScore++;
+        }
+    }
+}
+
+playRound(playerSelection, computerSelection);
+
+console.log(computerSelection);
+console.log(`Player score is ${playerScore}`);
+console.log(`Computer score is ${computerScore}`);
